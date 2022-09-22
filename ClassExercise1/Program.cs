7a34b61f390a0691a5b02861ed23118d7aa2837c
MadScientist586 <mr.scientist.knight@gmail.com>
2022-09-22 11:17:01 -0500

    Made a class for a car

---
+++ ClassExercise1/Program.cs
@@ -0,0 +1,9 @@
+﻿using ClassExercise1;
+
+Car myDreamCar = new Car();
+myDreamCar.Make = "Lamborghini";
+myDreamCar.Model = "Huracan Evo RWD";
+myDreamCar.Year = 2020;
+Console.WriteLine($"The make of my dream car is {myDreamCar.Make}");
+Console.WriteLine($"The model of my dream car is {myDreamCar.Model}");
+Console.WriteLine($"The year my dream car was made was {myDreamCar.Year}");
